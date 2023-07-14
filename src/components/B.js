f4d9eee60bc50122444d1c8f29cf94898742fbf9
kimdoeun <kdeun1@ex-em.com>
2023-07-14 22:15:26 +0900

    use React.memo()

--- src/components/B.js
+++ src/components/B.js
@@ -1,23 +1,25 @@
-const Message = ({ message }) => {
-  return <p>{message}</p>;
-};
+import { memo } from 'react';
 
-const ListItem = ({ post }) => {
+const Message = memo(({ message }) => {
+  return <p>{message}</p>;
+});
+
+const ListItem = memo(({ post }) => {
   return (
     <li key={post.id}>
       <p>{post.title}</p>
     </li>
   );
-};
+});
 
-const List = ({ posts }) => {
+const List = memo(({ posts }) => {
   return (
     <ul>
       {posts?.length &&
         posts.map(post => <ListItem key={post.id} post={post} />)}
     </ul>
   );
-};
+});
 
 const B = ({ message, posts }) => {
   return (
